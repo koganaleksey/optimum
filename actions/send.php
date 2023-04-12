337cc6de999d7326e4e874c7f8bc10ced639d580
Kogan Alexey <koganaleksey@mail.ru>
2023-04-13 01:04:55 +0300

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,5 +1,5 @@
 <?php
-$to = 'info@omtuae.com'; // <– replace with your address here
+$to = 'koganaleksey@mail.ru'; // <– replace with your address here
 $subject = 'Письмо от ';
 $message = 'Hello! This is a simple test email message.';
 $from = 'sender@emailaddress.here';
