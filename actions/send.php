9e3e701f8b8a0860a59e41e40029793aa64dde76
Kogan Alexey <koganaleksey@mail.ru>
2023-04-13 01:37:18 +0300

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -1,9 +1,40 @@
 <?php
-$to = 'koganaleksey@mail.ru'; // <– replace with your address here
-$subject = 'Письмо от ';
-$message = 'Hello! This is a simple test email message.';
-$from = 'sender@emailaddress.here';
-$headers = 'From:' . $from;
-mail($to, $subject, $message, $headers);
-echo 'Mail Sent.';
-?>
+require 'PHPMailer/class.phpmailer.php';
+
+if (isset($_POST['send'])) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $msg = $_POST['message'];
+   $subject = 'Message from omtuae.com';
+
+   $mail = new PHPMailer(true);
+
+   $mail->IsSMTP();
+   $mail->SMTPAuth = false;
+   $mail->Port = 25;
+   $mail->Host = "localhost";
+   $mail->Username = "info@omtuae.com";
+   $mail->Password = "UiYYiiPae21%";
+
+   $mail->IsSendmail();
+
+   $mail->From = "omtuae.com";
+   $mail->FromName = $name;
+
+   $mail->AddAddress($email);
+   $mail->Subject = $subject;
+   $mail->WordWrap = 80;
+
+   $mail->MsgHTML($msg);
+   $mail->IsHTML(true);
+
+   if (!$mail->Send()) {
+      echo "Mail Not Sent";
+   } else {
+      echo '<script language="javascript">';
+      echo 'alert("Thank You Contacting Us We Will Response You As Early Possible")';
+      echo '</script>';
+
+   }
+
+}
