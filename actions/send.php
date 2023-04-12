3c0c13d4e19575ed83e20c4ba2a3333422ab80b9
Kogan Alexey <koganaleksey@mail.ru>
2023-04-12 20:11:51 +0300

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -10,7 +10,7 @@ $success_message = '<div class="d-flex justify-content-center align-items-center
 </div>';
 
 $mail = array(
-  'to' => "koganaleksey@mail.ru",
+  'to' => "info@omtuae.com",
   'subject' => "Message from omtuae.com",
   'message' => "Имя: " . $name . "\n\n" . "Email: " . $email_from . "\n\n" . "Сообщение: " . "\r\n" . $message,
   'headers' => "MIME-Version: 1.0\r\n" . "Content-type: text/plain; charset=utf-8\r\n" . "From: <omtuae.com>\r\n"
